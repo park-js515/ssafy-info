75a50f14f80c9263834376ae4ed38395235e87b6
Juseong <hdcom37@gmail.com>
2023-07-18 23:45:37 +0900

    0718(1)

--- React/Nomad1/my-react-app/src/App.js
+++ React/Nomad1/my-react-app/src/App.js
@@ -1,42 +1,23 @@
-import { useState, useEffect } from "react";
+import {
+	BrowserRouter as Router,
+	Routes, // Switch -> Routes
+	Route,
+	// Link
+} from "react-router-dom";
+
+import Home from "./routes/Home.js"
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
-    );
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  console.log(movies);
 
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <h2>{movie.title}</h2>
-							<p>{movie.summary}</p>
-							<ul>
-								{movie.genres.map((g) => <li key={g}>{g}</li>)}
-							</ul>
-							<img src={movie.medium_cover_image} alt={movie.title} />
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+		<Router>
+			<Routes>
+				<Route path="/hello" element={<h1>Hello!</h1>}></Route>
+				<Route path="/movie/:id" element={<Detail/>}></Route>
+				<Route path="/" element={<Home />}></Route>
+			</Routes>
+		</Router>
   );
 }
 
